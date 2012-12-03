7bfaff6ac4c4bd65c1694b7c548a744357f966b0
dev001 <dev001@Cubet>
2012-12-03 16:13:17 +0530

    PHP Fatal error:  Cannot re-assign auto-global variable _POST issue solved and issue with the site configuration settings also saved.

--- application/models/admin_model.php
+++ application/models/admin_model.php
@@ -1,21 +1,22 @@
 <?php
-/**
-Action model to handles the administrative functions
 
-* @package pinterest clone model
-* @subpackage
-* @uses : To handle all the  actions of a pin. Email, embed , report
-* @version $id:$
-* @since 03-05-2012
-* @author Vishal Vijayan
-* @copyright Copyright (c) 2007-2010 Cubet Technologies. (http://cubettechnologies.com)
-*/
+/**
+  Action model to handles the administrative functions
+
+ * @package pinterest clone model
+ * @subpackage
+ * @uses : To handle all the  actions of a pin. Email, embed , report
+ * @version $id:$
+ * @since 03-05-2012
+ * @author Vishal Vijayan
+ * @copyright Copyright (c) 2007-2010 Cubet Technologies. (http://cubettechnologies.com)
+ */
 class Admin_model extends CI_Model {
 
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct() {
+        parent::__construct();
+    }
+
     /**
      * Function to handle the admin login page.
      * @param  :
@@ -23,8 +24,7 @@ class Admin_model extends CI_Model {
      * @since  : 22-05-2012
      * @return
      */
-    function adminLogin($username,$password)
-    {
+    function adminLogin($username, $password) {
         $sql = "SELECT * 
                     FROM
                         admin_users
@@ -32,36 +32,34 @@ class Admin_model extends CI_Model {
                         username='$username'
                     AND
                         password='$password' ";
-        $query      =  $this->db->query($sql);
-        if($query->num_rows>0)
-        {   $row    =  $query->row();
+        $query = $this->db->query($sql);
+        if ($query->num_rows > 0) {
+            $row = $query->row();
             return $row;
-        }
-        else{
+        } else {
             return false;
         }
     }
-     /**
+
+    /**
      * Function to handle the users . Display all users with edit and delete option
      * @param  :
      * @author : Vishal
      * @since  : 22-05-2012
      * @return
      */
-    function getUsers()
-    {
+    function getUsers() {
         $sql = "SELECT *
                     FROM
                         user";
-        $query =  $this->db->query($sql);
-        if($query->num_rows>0)
-        {  
+        $query = $this->db->query($sql);
+        if ($query->num_rows > 0) {
             return $query->result();
-        }
-        else{
+        } else {
             return false;
         }
     }
+
     /**
      * Function to get user details by id
      * @param  :
@@ -69,35 +67,33 @@ class Admin_model extends CI_Model {
      * @since  : 22-05-2012
      * @return
      */
-    function getUsersById($id)
-    {
-        $sql= "SELECT *
+    function getUsersById($id) {
+        $sql = "SELECT *
                     FROM
                         user
                     WHERE
                         id = {$id}";
-        $query =  $this->db->query($sql);
-        if($query->num_rows>0)
-        {
+        $query = $this->db->query($sql);
+        if ($query->num_rows > 0) {
             return $query->row();
-        }
-        else{
+        } else {
             return false;
         }
     }
-      /**
+
+    /**
      * Function to save the user after edit
      * @param  :
      * @author : Vishal
      * @since  : 22-05-2012
      * @return
      */
-    function saveEditUser($id,$user)
-    {
-        $this->db->where('id',$id);
-        $this->db->update('user',$user);
+    function saveEditUser($id, $user) {
+        $this->db->where('id', $id);
+        $this->db->update('user', $user);
         return true;
     }
+
     /**
      * Function to all pins from the pin table, may or may not user id provided
      * @since 03-05-2012
@@ -105,35 +101,32 @@ class Admin_model extends CI_Model {
      * @param <Int> $userID;
      * @return <int> id
      */
-    function getAllPins($userID=false,$order=false,$offset,$limit)
-    {
+    function getAllPins($userID = false, $order = false, $offset, $limit) {
         $sql = "SELECT * 
                     FROM
                         pins";
 
-        if($userID)
+        if ($userID)
             $sql .= " WHERE 
                         user_id = $userID ";
 
-        if($order)
-        {
+        if ($order) {
             $sql .= " ORDER BY
                         ' $order'";
-        }
-        else{
+        } else {
             $sql .= " ORDER BY
                         RAND()";
         }
         $sql.=" LIMIT $offset,$limit";
 
 
-        $query      = $this->db->query($sql);
-        if($query->num_rows()>0)
-        {
-            $row    = $query->result();
+        $query = $this->db->query($sql);
+        if ($query->num_rows() > 0) {
+            $row = $query->result();
             return $row;
         }
     }
+
     /**
      * Function to get the count of pins from pins table based on userid or get all the pins count irrespective of user
      * @since 22-05-2012
@@ -141,20 +134,17 @@ class Admin_model extends CI_Model {
      * @param <Int> $userid;
      * @return 
      */
-    function getAllPinsCount($userid=false)
-    {
-        if($userid)
-        {
+    function getAllPinsCount($userid = false) {
+        if ($userid) {
             $this->db->select('id');
-            $this->db->where('user_id',$userid);
+            $this->db->where('user_id', $userid);
             $query = $this->db->get('pins');
             return $query->num_rows();
-        }
-        else{
+        } else {
             return $this->db->count_all('pins');
         }
-        
     }
+
     /**
      * Function to get all the email ids of the users from user table.
      * @since 22-05-2012
@@ -162,16 +152,16 @@ class Admin_model extends CI_Model {
      * @param 
      * @return
      */
-    function getUsersEmail()
-    {
+    function getUsersEmail() {
         $this->db->select('email');
         $query = $this->db->get('user');
-        $result =  $query->result();
+        $result = $query->result();
         foreach ($result as $key => $value) {
-            $return[$key]=$value->email;
+            $return[$key] = $value->email;
         }
         return $return;
     }
+
     /**
      * Function to save the editted pins back to the pins table
      * @since 22-05-2012
@@ -179,21 +169,20 @@ class Admin_model extends CI_Model {
      * @param  $update,$id
      * @return
      */
-    function saveEdittedPin($update,$id)
-    {
+    function saveEdittedPin($update, $id) {
         $this->db->where('id', $id);
         $this->db->update('pins', $update);
         return true;
     }
-     /**
+
+    /**
      * Function to search for the user to autocomplete the user search input box
      * @since 22-05-2012
      * @author Vishal Vijayan
      * @param  $q
      * @return
      */
-    function search($q)
-    {
+    function search($q) {
         $sql = "SELECT 
                         id,
                         image,
@@ -206,6 +195,7 @@ class Admin_model extends CI_Model {
         $query = $this->db->query($sql);
         return $query->result();
     }
+
     /**
      * Function delete an pin
      * @param  : $pinId
@@ -213,8 +203,7 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function deletePin($pinId)
-    {
+    function deletePin($pinId) {
 
         $this->db->where('new_pin_id', $pinId);
         $this->db->delete('repin');
@@ -225,7 +214,7 @@ class Admin_model extends CI_Model {
         $this->db->where('action_id', $pinId);
         $this->db->delete('activity');
 
-       
+
 
         $this->db->where('pin_id', $pinId);
         $this->db->delete('comments');
@@ -243,6 +232,7 @@ class Admin_model extends CI_Model {
         $this->db->query($sql);
         return true;
     }
+
     /**
      * Function the uploaded pin
      * @param  : $insert
@@ -250,11 +240,11 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveUploadPin($insert)
-    {
+    function saveUploadPin($insert) {
         $this->db->insert('pins', $insert);
         return $this->db->insert_id();
     }
+
     /**
      * Function to get the category
      * @param  : $insert
@@ -262,16 +252,16 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getCategory($limit,$offset)
-    {
-        $sql    = "SELECT
+    function getCategory($limit, $offset) {
+        $sql = "SELECT
                         *
                     FROM
                         category_list
                     LIMIT $offset,$limit";
-        $query  = $this->db->query($sql);
+        $query = $this->db->query($sql);
         return $query->result();
     }
+
     /**
      * Function to get the category by id
      * @param  : $id
@@ -279,17 +269,17 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getCategoryById($id)
-    {
-        $sql    = "SELECT
+    function getCategoryById($id) {
+        $sql = "SELECT
                         *
                     FROM
                         category_list
                     WHERE
                         id=$id";
-        $query  = $this->db->query($sql);
+        $query = $this->db->query($sql);
         return $query->row();
     }
+
     /**
      * Function to save the editted category
      * @param  : $id,$update
@@ -297,12 +287,12 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveEdittedCategory($id,$update)
-    {
+    function saveEdittedCategory($id, $update) {
         $this->db->where('id', $id);
         $this->db->update('category_list', $update);
         return true;
     }
+
     /**
      * Function to save the new category
      * @param  : $insert
@@ -310,24 +300,24 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveNewCategory($insert)
-    {
+    function saveNewCategory($insert) {
         $this->db->insert('category_list', $insert);
         return $this->db->insert_id();
     }
-     /**
+
+    /**
      * Function to delete a category
      * @param  : $id
      * @author : Vishal
      * @since  : 23-05-2012
      * @return :
      */
-    function deleteCategory($id)
-    {
+    function deleteCategory($id) {
         $sql = "DELETE FROM category_list WHERE id=$id";
         $this->db->query($sql);
         return true;
     }
+
     /**
      * Function to get the count of boards
      * @param  : $userId
@@ -335,20 +325,18 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getAllBoardsCount($userId=false)
-    {
+    function getAllBoardsCount($userId = false) {
 
-        if($userId)
-        {
+        if ($userId) {
             $this->db->select('id');
-            $this->db->where('user_id',$userId);
+            $this->db->where('user_id', $userId);
             $query = $this->db->get('board');
             return $query->num_rows();
-        }
-        else{
+        } else {
             return $this->db->count_all('board');
         }
     }
+
     /**
      * Function to get all boards
      * @param  : $userID=false,$order=false,$offset,$limit
@@ -356,32 +344,29 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getAllBoards($userID=false,$order=false,$offset,$limit)
-    {
+    function getAllBoards($userID = false, $order = false, $offset, $limit) {
         $sql = "SELECT * from board";
 
-        if($userID)
+        if ($userID)
             $sql .= " WHERE user_id= $userID ";
 
-        if($order)
-        {
+        if ($order) {
             $sql .= " ORDER BY
                         ' $order'";
-        }
-        else{
+        } else {
             $sql .= " ORDER BY
                         RAND()";
         }
         $sql.=" LIMIT $offset,$limit";
 
 
-        $query      = $this->db->query($sql);
-        if($query->num_rows()>0)
-        {
-            $row    = $query->result();
+        $query = $this->db->query($sql);
+        if ($query->num_rows() > 0) {
+            $row = $query->result();
             return $row;
         }
     }
+
     /**
      * Function to save the editted board
      * @param  : $update,$id
@@ -389,12 +374,12 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveEdittedBoard($update,$id)
-    {
+    function saveEdittedBoard($update, $id) {
         $this->db->where('id', $id);
         $this->db->update('board', $update);
         return true;
     }
+
     /**
      * Function to delete a board
      * @param  : $boardId
@@ -402,17 +387,16 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function deleteBoard($boardId)
-    {
+    function deleteBoard($boardId) {
 
-        $sql    = "SELECT
+        $sql = "SELECT
                         *
                     FROM
                         pins
                     WHERE
                         board_id = $boardId";
-        $query  = $this->db->query($sql);
-        $pinDetails =  $query->row();
+        $query = $this->db->query($sql);
+        $pinDetails = $query->row();
         foreach ($pinDetails as $key => $value) {
             $this->db->where('new_pin_id', $value->id);
             $this->db->delete('repin');
@@ -423,7 +407,7 @@ class Admin_model extends CI_Model {
             $this->db->where('action_id', $value->id);
             $this->db->delete('activity');
 
-            
+
             $this->db->where('pin_id', $value->id);
             $this->db->delete('comments');
 
@@ -432,9 +416,6 @@ class Admin_model extends CI_Model {
 
             $this->db->where('id', $value->id);
             $this->db->delete('pins');
-
-           
-
         }
         $sql = "DELETE FROM board WHERE id=$boardId";
         $this->db->query($sql);
@@ -444,9 +425,10 @@ class Admin_model extends CI_Model {
 
         $this->db->where('board_id', $boardId);
         $this->db->delete('report_pins');
-        
+
         return true;
     }
+
     /**
      * Function to update a user status
      * @param  : $update,$id
@@ -454,12 +436,12 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function updateUserStatus($update,$id)
-    {
+    function updateUserStatus($update, $id) {
         $this->db->where('id', $id);
         $this->db->update('user', $update);
         return true;
     }
+
     /**
      * Function to create a new board
      * @param  : $insert
@@ -467,11 +449,11 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function createNewBoard($insert)
-    {
+    function createNewBoard($insert) {
         $this->db->insert('board', $insert);
         return $this->db->insert_id();
     }
+
     /**
      * Function to get the current settings of the site
      * @param  :
@@ -479,13 +461,25 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getSiteSettings()
-    {
+    function getSiteSettings() {
         $sql = "SELECT * FROM config";
-        $query  = $this->db->query($sql);
+        $query = $this->db->query($sql);
         return $query->row_array();
-
     }
+
+    /**
+     * Function to save the settings of the site
+     * @param  :
+     * @author : Rahul K Murali
+     * @since  : 03-12-2012
+     * @return :
+     */
+    function saveSettings($insert) {
+
+        $this->db->insert('config', $insert);
+        return $this->db->insert_id();
+    }
+
     /**
      * Function to save the settings of the site
      * @param  :
@@ -493,12 +487,11 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveSettings($post,$id)
-    {
+    function updateSettings($data, $id) {
         $this->db->where('id', $id);
-        $this->db->update('config', $post);
-        return true;
+        return $this->db->update('config', $data);
     }
+
     /**
      * Function to get the pins reported
      * @param  :
@@ -506,41 +499,38 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function getReportedPins()
-    {
+    function getReportedPins() {
         $sql = "select * from report_pins";
         $query = $this->db->query($sql);
-        if($query->num_rows()>0)
-        {
+        if ($query->num_rows() > 0) {
             return $query->result();
-        }
-        else{
+        } else {
             return false;
         }
     }
-     /**
+
+    /**
      * Function to get details of a admin
      * @param  :
      * @author : Vishal
      * @since  : 23-05-2012
      * @return :
      */
-    function getAdminDetails()
-    {
+    function getAdminDetails() {
         $sql = "SELECT *
                     FROM
                         admin_users
                     WHERE
                         id = {$this->session->userdata('admin_login_id')}";
         $query = $this->db->query($sql);
-        if($query->num_rows()>0)
-        {   $row = $query->row();
+        if ($query->num_rows() > 0) {
+            $row = $query->row();
             return $row;
-        }
-        else{
+        } else {
             return false;
         }
     }
+
     /**
      * Function to save back the admin details
      * @param  : $update
@@ -548,11 +538,12 @@ class Admin_model extends CI_Model {
      * @since  : 23-05-2012
      * @return :
      */
-    function saveAdminDetails($update)
-    {
+    function saveAdminDetails($update) {
         $this->db->where('id', $this->session->userdata('admin_login_id'));
         $this->db->update('admin_users', $update);
         return true;
     }
+
 }
+
 ?>
